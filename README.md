3d575e09420a6bce563e76a83df6c0eec5eaa943
Andrzej Budzanowski <psychob.pl@gmail.com>
2015-05-14 15:56:34 +0200

    added arg-inspect

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This repo contains simple utilitie programs i wrote. They made sense to write them at some point in time. Right now they can make no sense at all. Have fun
 
 Small application that don't use more then one file are stored in main directory, and more "*complex*" applications are stored in separated directories
+
+   * arg-inspect - simple program that only outputs its arguments, written in c++
---
+++ arg-inspect.cpp
@@ -0,0 +1,24 @@
+﻿//
+// arg-inspect
+// (c) 2015 Andrzej Budzanowski
+//
+
+#include <iostream>
+
+int main( int argc, const char * argv[] )
+{
+ if ( argc == 1 )
+ {
+  std::cout << "arg-inspect\n"
+               "(c) 2015 Andrzej Budzanowski\n"
+               "--\n"
+               "No argument parameters to list" << std::endl;
+
+  return 1;
+ }
+
+ for ( int it = 1; it < argc; ++it )
+  std::cout << it << ": " << argv[it] << std::endl;
+
+ return 0;
+}
